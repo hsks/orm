d419aa359a41835f12ffa721320d0cd09acd42da
nkg-laufire <nkg@laufire.in>
2022-06-14 12:44:24 +0530

    Fetched children data from db.

--- src/cb.js
+++ src/cb.js
@@ -1,11 +1,43 @@
 const { sleep, peek } = require('@laufire/utils/debug');
+const { idKey, statusKey } = require('./constants');
+
+const deliveryAddress = {
+	[idKey]: '',
+	city: '',
+	pincode: '',
+	street: '',
+	[statusKey]: 'create',
+};
+const products = [
+	{
+		[idKey]: '',
+		name: '',
+		quantity: '',
+		price: 1,
+		[statusKey]: 'create',
+	},
+];
+const apiData = {
+	deliveryAddress: deliveryAddress,
+	products: products,
+	orders: [
+		{
+			[idKey]: '',
+			date: '',
+			invoiceNumber: '',
+			deliveryDate: '',
+			[statusKey]: 'delete',
+		},
+	],
+};
 
 const cb = async ({ entityName, action, data }) => {
 	peek({ entityName, action, data });
 	const duration = 2000;
 
 	await sleep(duration);
-	return data;
+
+	return apiData[entityName] || data;
 };
 
 module.exports = cb;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,38 @@ const parser = require('./parser');
 const source = require('./structure');
 const config = require('./config');
 const cb = require('./cb');
+const fetchTrial = require('./builtEntity');
+const { idKey, statusKey } = require('./constants');
 
 parser({ source, config, cb });
+
+fetchTrial({
+	source: {
+		customer: {
+			[idKey]: '',
+			[statusKey]: 'fetch',
+		},
+	},
+	config: {
+		customer: {
+			statusKey: statusKey,
+			type: 'entity',
+			children: {
+				orders: {
+					type: 'collection',
+					children: {
+						deliveryAddress: {
+							type: 'entity',
+							children: {},
+						},
+						products: {
+							type: 'collection',
+							children: {},
+						},
+					},
+				},
+			},
+		},
+	},
+	cb: cb,
+});
