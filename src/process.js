0d21e4c62994d26d8247b5c76892561f7273d385
nkg-laufire <nkg@laufire.in>
2022-06-14 12:41:44 +0530

    Refactored.

--- src/process.js
+++ src/process.js
@@ -1,21 +1,17 @@
-const { map, result } = require('@laufire/utils/collection');
-const { resolve } = require('@laufire/utils/path');
+const { map } = require('@laufire/utils/collection');
 const { isDefined } = require('@laufire/utils/reflection');
+const { statusKey, idKey } = require('./constants');
 const getAction = require('./getAction');
 
 // eslint-disable-next-line max-lines-per-function
 const entity = (context) => {
 	const {
-		source,
-		config,
 		data: {
 			entityData,
-			entityName,
 			config: currentConfig,
 			parentStatus = 'sync',
 		},
 	} = context;
-	const { statusKey, id } = config;
 	const { [statusKey]: currentStatus } = entityData;
 	const { children } = currentConfig;
 
@@ -24,62 +20,62 @@ const entity = (context) => {
 		data: {
 			parentStatus: parentStatus,
 			currentStatus: currentStatus,
-			idExists: isDefined(entityData[id]),
+			idExists: isDefined(entityData[idKey]),
 		},
 	});
 
-	// eslint-disable-next-line no-use-before-define
-	map(children, ({ path }, name) => process({
-		...context,
-		data: {
-			entityData: result(source[entityName], path),
-			entityName: name,
-			config: result(currentConfig, resolve(`children/${ path }`)),
-			parentStatus: entityData[statusKey],
-		},
-	}));
-};
-
-// eslint-disable-next-line max-lines-per-function
-const collection = (context) => {
-	const { config: { statusKey, id }} = context;
-	const { data: { parentStatus, entityData, config }} = context;
-	const { children } = config;
-
-	// eslint-disable-next-line max-lines-per-function
-	map(entityData, (item) => {
-		const { [statusKey]: currentStatus } = item;
-
-		item[statusKey] = getAction({
-			...context,
-			data: {
-				parentStatus: parentStatus,
-				currentStatus: currentStatus,
-				idExists: isDefined(item[id]),
-			},
-		});
+	map(children, (config, name) => {
+		const { type } = config;
 
 		// eslint-disable-next-line no-use-before-define
-		map(children, (data, name) => process({
+		typeProcessors[type]({
 			...context,
 			data: {
-				config: config.children[name],
-				entityData: item[name],
+				entityData: entityData[name],
 				entityName: name,
-				parentStatus: item[statusKey],
+				config: config,
+				parentStatus: currentStatus,
+			},
+		});
+	});
+};
+
+const collection = (context) => {
+	const { data: { entityName, entityData, config, parentStatus }} = context;
+
+	map(entityData, (data) =>
+		entity({
+			...context,
+			data: {
+				entityName: entityName,
+				entityData: data,
+				config: config,
+				parentStatus: parentStatus,
 			},
 		}));
-	});
+};
+
+const typeProcessors = {
+	entity,
+	collection,
 };
 
 const process = (context) => {
-	const { data: { config: { type }}} = context;
-	const typeProcessors = {
-		entity,
-		collection,
-	};
+	const { source, config: { children }} = context;
 
-	typeProcessors[type](context);
+	map(source, (entityData, entityName) => {
+		const entityConfig = children[entityName];
+		const { type } = entityConfig;
+
+		return typeProcessors[type]({
+			...context,
+			data: {
+				entityData: entityData,
+				entityName: entityName,
+				config: entityConfig,
+			},
+		});
+	});
 };
 
 module.exports = process;
